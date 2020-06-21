5e784844029828d69c37088da67da1c1cad808c1
Vinicius Gubiani Ferreira <vini.g.fer@gmail.com>
2020-06-21 16:23:39 -0300

    dummy task

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-//This is a comment
-console.log("Hello world!\n");
+var express = require("express");
+app = express();
+
+app.get('/', function(req, res){
+    res.send('hello world!!\n');
+});
+
+app.get('/mars', function(req, res){
+    res.send('hello mars!\n');
+});
